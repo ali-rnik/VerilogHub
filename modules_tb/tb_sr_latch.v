d21c2740214dab2ff2f5afc4b768080d5c457c67
Ali R. Nik <strongsaxophone@gmail.com>
2019-07-12 17:09:27 +0430

    modifying Makefile and change file heirarchy

--- modules_tb/tb_sr_latch.v
+++ modules_tb/tb_sr_latch.v
@@ -7,7 +7,7 @@ module tb_sr_latch;
 	sr_latch M1(t_in[2], t_in[1], t_in[0], out);
 
 	initial begin
-		$dumpfile("test_sr_latch.vcd");
+		$dumpfile("tb_sr_latch.vcd");
 		$dumpvars(0);
 		t_in = 3'b000;
 		repeat(8) begin
