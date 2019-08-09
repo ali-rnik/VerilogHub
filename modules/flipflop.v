803057bce8db5e4ce8196165ed3d31521e7fd731
Ali R. Nik <strongsaxophone@gmail.com>
2019-08-09 18:20:46 +0430

    deleted unnecessary files

--- modules/flipflop.v
+++
@@ -1,7 +0,0 @@
-`include "latch.v"
-
-module FLIPFLOP(input data, clk, preset, clear, output q, qbar);
-	wire q1;
-	LATCH D1(data, !clk, preset, clear, q1,);
-	LATCH D2(q1, clk, preset, clear, q, qbar);
-endmodule
