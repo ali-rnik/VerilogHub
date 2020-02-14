0fb844a954c22b8f66378f8e5a363beb647cfd0d
Ali R. Nik <strongsaxophone@gmail.com>
2020-02-14 11:27:11 +0330

    lw sw beq add addi implemented with extra instruction memory

--- modules/instruction_register.v
+++ modules/instruction_register.v
@@ -9,8 +9,22 @@ module instruction_register (input [63:0] in, input clk, output [63:0] out);
 
 //------------------------simulation-------------------------------//
 	always @(*) begin
-		$display("instruction_register: data:    %2d", data);
-		$display("instruction_register: in:  %2d", in);
-		$display("instruction_register: out: %2d\n", out);
+		$display("R-type:");
+		$display("instruction_register: op:       %2d", out[5:0]);
+		$display("instruction_register: dst_adr:  %2d", out[11:6]);
+		$display("instruction_register: src1_adr: %2d", out[17:12]);
+		$display("instruction_register: src2_adr: %2d\n", out[23:18]);
+
+		$display("I-type: beq");
+		$display("instruction_register: op:       %2d", out[5:0]);
+		$display("instruction_register: src1_adr: %2d", out[11:6]);
+		$display("instruction_register: src2_adr: %2d", out[17:12]);
+		$display("instruction_register: jump_adr: %2d\n", out[33:18]);
+
+		$display("I-type: addi");
+		$display("instruction_register: op:       %2d", out[5:0]);
+		$display("instruction_register: dst_adr:  %2d", out[11:6]);
+		$display("instruction_register: src1_adr: %2d", out[17:12]);
+		$display("instruction_register: imd_val:  %2d\n", out[63:18]);
 	end
 endmodule
